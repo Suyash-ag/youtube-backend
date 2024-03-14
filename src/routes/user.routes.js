f2d016fa10ae6b8433543e56ee0a214f982a9ed8
Suyash-ag <143213011+Suyash-ag@users.noreply.github.com>
2024-03-14 16:23:54 +0530

    all setup configured

---
+++ src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import registerUser from "../controllers/user.controller.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+// router.route("/login").post(login);
+
+export default router;
